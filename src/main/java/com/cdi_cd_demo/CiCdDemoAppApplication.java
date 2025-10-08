452d4751f54de462a1bd0db479b3ba9d3209c90e
urvish16 <urvishsoni96@gmail.com>
2025-10-08 21:34:52 +0530

    Initial commit of Spring Boot demo

---
+++ src/main/java/com/cdi_cd_demo/CiCdDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.cdi_cd_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdi_cd_demo/CiCdDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdi_cd_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
